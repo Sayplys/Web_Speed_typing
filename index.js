bf8b70e998724342eb46850ded5193eb487d1c70
Sayplys <p3drov1n1cyus@gmail.com>
2024-01-25 11:16:55 -0300

    adding more status and randomized texts

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const startTime = 30
+const startTime = 60
 const timeVariation = 100
-const texts = ['test <>. text', "text text² 123"]
+const texts = ['Did you know that honey never spoils? Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible. The long shelf life is due to its low moisture content and acidic pH, creating an inhospitable environment for bacteria and microorganisms.', 
+"In space, there's a giant diamond star named BPM 37093, located 50 light-years away from Earth. Dubbed 'Lucy' after the Beatles song 'Lucy in the Sky with Diamonds,' it's a crystallized white dwarf with a core of carbon and oxygen, estimated to be ten billion trillion trillion carats, making it the largest diamond ever discovered.",
+"Quantum computers can perform complex calculations at unimaginable speeds due to the principles of superposition and entanglement. Unlike classical bits, which can only exist in states of 0 or 1, quantum bits (qubits) can exist in both states simultaneously, exponentially increasing computational power. This enables quantum computers to solve certain problems much faster than traditional computers."]
 
 var timer = startTime
 var isGameRunning = false
 var words = 1
 var wpm = 0
+var accurancy = 100
+var points = 0
 var textMap = []
 
 const targetTextElement = $("#target-text")
 const textInputElement = $("#text-input")
 
 function start(){
-    targetTextElement.text(texts[0])
+    targetTextElement.text(texts[Math.floor(Math.random() * texts.length)])
     textInputElement.val('')
     timer = startTime
     isGameRunning = true
@@ -28,7 +32,9 @@ function update(){
         calcWPM()
         if(isGameRunning){
             update()
+            return
         }
+        calcPoints()
     }, timeVariation)
 }
 
@@ -68,6 +74,7 @@ function compare(){
         
         styledText += target.substring(current)
         targetTextElement.html(styledText)
+        calcAcurrancy()
     }
 
     if(target.length - 1 <= currentIndex){
@@ -80,14 +87,29 @@ function calcWPM(){
     $("#wpm-count").text(wpm.toFixed(2))
 }
 
+function calcAcurrancy(){
+    var correctQtd = 0
+    textMap.forEach((value)=>{if(value==true)correctQtd++})
+    accurancy = (correctQtd * 100) / textMap.length
+    $("#accurancy-count").text(accurancy.toFixed(2))
+}
+
+function calcPoints(){
+    points = wpm * accurancy**3
+    $("#points-count").text((points/1000).toFixed())
+}
+
 textInputElement.on("keydown", (value)=>{
-    if(value.key == "Backspace") {
+    if(isGameRunning){
+        if(value.key == "Backspace") {
         textMap.pop()
         setTimeout(()=>{compare()},1)
-    }else if(value.key == " "){
-        words++
-    }
+        }else if(value.key == " "){
+            words++
+        }
 
-    setTimeout(()=>{compare()},1)
+        setTimeout(()=>{compare()},1)
+    }
+    
 })
 
